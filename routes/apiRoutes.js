7ef1412cc5fef254153df38115d512de9d878e82
Joey Sisk <jjmsisk@gmail.com>
2021-02-18 15:22:23 -0600

    fixed the gitignore, progress with routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,66 +1,64 @@
 const { Workout } = require("../models");
 
-const router = require("express").Router();
+module.exports = function (app) {
+  // getLastWorkout --find all--
+  app.get("/api/workouts/:id", (req, res) => {
+    Workout.find({})
+      .then((workouts) => {
+        res.json(workouts);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
 
-// getLastWorkout --find all--
-router.get("/api/workouts/:id", (req, res) => {
-  Workout.find({})
-    .then((workouts) => {
-      res.json(workouts);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-// addExercise PUT req.params.id
-router.put("/workouts/:id", (req, res) => {
-  Workout.findByIdAndUpdate(
-    req.params.id,
-    {
-      $push: {
-        exercise: {
-          name: req.body.type,
-          type: req.body.name,
-          weight: req.body.weight,
-          sets: req.body.sets,
-          reps: req.body.reps,
-          duration: req.body.duration,
-          distance: req.body.distance,
+  // addExercise PUT req.params.id
+  app.put("/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          exercise: {
+            name: req.body.type,
+            type: req.body.name,
+            weight: req.body.weight,
+            sets: req.body.sets,
+            reps: req.body.reps,
+            duration: req.body.duration,
+            distance: req.body.distance,
+          },
         },
       },
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
+      (error, data) => {
+        if (error) {
+          res.send(error);
+        } else {
+          res.send(data);
+        }
       }
-    }
-  );
-});
+    );
+  });
 
-// createWorkout POST req.query.params
-router.post("/api/workouts", ({ body }, res) => {
-  const workout = new Workout(body);
-  Workout.create(body)
-    .then((workouts) => {
-      res.json(workouts);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
+  // createWorkout POST req.query.params
+  app.post("/api/workouts", ({ body }, res) => {
+    const workout = new Workout(body);
+    Workout.create(body)
+      .then((workouts) => {
+        res.json(workouts);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
 
-// getWorkoutsInRange --find all, aggregration of duration-- --same as first get--
-router.get("/api/workouts/range", (req, res) => {
-  Workout.find({})
-    .then((workouts) => {
-      res.json(workouts);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-module.exports = router;
+  // getWorkoutsInRange --find all, aggregration of duration-- --same as first get--
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then((workouts) => {
+        res.json(workouts);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,10 @@ var path = require("path");
 
 module.exports = function (app) {
   app.get("/stats", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats"));
   });
 
   app.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise"));
   });
 };
