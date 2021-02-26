0064ff805632ee7dd4f3e22551a91a452b05b3ca
Joey Sisk <jjmsisk@gmail.com>
2021-02-25 20:16:33 -0600

    more heroku testing, server file

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require("express"); // server framework
+const mongoose = require("mongoose"); // handles the mongodb
+const logger = require("morgan"); // logs api calls to terminal
+const app = express(); // for communicating with server
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // establishes location of connection
 
-const db = require("./models");
+// const db = require("./models");
 
-const app = express();
-
-app.use(logger("dev"));
-
+app.use(logger("dev")); // these run after request, before routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// neccessary to properly run mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// provides more information on db errors in terminal
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("connected to Database"));
 
-require("./routes/html-routes")(app);
+// connect to html and api routes
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
+// connects to db propper and logs location
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
